90be3d89ee98c1be2e65e6dcc08514897b47ccf4
Sam Perry <u1265119@unimail.hud.ac.uk>
2017-02-10 11:22:07 +0000

    Finished 2nd order crossover prototype

---
+++ CrossoverTest.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import numpy as np
+import pdb
+import scipy.signal as signal
+import matplotlib.pyplot as plt
+
+def main():
+    # Sampling frequency
+    fs = 10000
+    # Cutoff frequency in hertz
+    cutoff_freq = 1000.
+
+    # Calculate ratio between cutoff frequency and sampling rate
+    wc = cutoff_freq/fs
+
+    # Deifine Q as the square root of 2
+    q=np.sqrt(2.0)
+
+    # Warp the frequency to convert from continuous to discrete time cutoff
+    wd1 = 1.0 / np.tan(np.pi*wc)
+
+    # Calculate coefficients from equation
+    b0 = 1.0 / (1.0 + q*wd1 + wd1**2)
+    b1 = 2*b0
+    b2 = b0
+    a1 = -2.0 * (wd1**2 - 1.0) * b0
+    a2 = (1.0 - q*wd1 + wd1**2) * b0
+
+    w, h = signal.freqz([b0, b1, b2], [1, a1, a2], plot = lambda w, h: plt.plot((fs * 0.5 / np.pi) * w, 20 * np.log10(abs(h))))
+    b0 = b0*wd1**2;
+    b1 = -b1*wd1**2;
+    b2 = b2*wd1**2;
+    w, h = signal.freqz([b0, b1, b2], [1, a1, a2], plot = lambda w, h: plt.plot((fs * 0.5 / np.pi) * w, 20 * np.log10(abs(h))))
+
+    plt.axvline(cutoff_freq, color='r')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
