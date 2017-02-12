5e9a189fdf2cd08a54f2aca40653dddb1789bd5d
Sam Perry <u1265119@unimail.hud.ac.uk>
2017-02-12 19:33:13 +0000

    Finished prototyping linkwitz-riley filter

--- CrossoverTest.py
+++ CrossoverTest.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 
 def main():
     # Sampling frequency
-    fs = 10000
+    fs = 44100
     # Cutoff frequency in hertz
-    cutoff_freq = 1000.
+    cutoff_freq = 5000.
 
     # Calculate ratio between cutoff frequency and sampling rate
     wc = cutoff_freq/fs
@@ -27,11 +27,19 @@ def main():
     a1 = -2.0 * (wd1**2 - 1.0) * b0
     a2 = (1.0 - q*wd1 + wd1**2) * b0
 
+    b_conv = np.convolve([b0, b1, b2], [b0, b1, b2])
+    a_conv = np.convolve([1, a1, a2], [1, a1, a2])
     w, h = signal.freqz([b0, b1, b2], [1, a1, a2], plot = lambda w, h: plt.plot((fs * 0.5 / np.pi) * w, 20 * np.log10(abs(h))))
+    w, h = signal.freqz(b_conv, a_conv, plot = lambda w, h: plt.plot((fs * 0.5 / np.pi) * w, 20 * np.log10(abs(h))))
+    pdb.set_trace()
     b0 = b0*wd1**2;
     b1 = -b1*wd1**2;
     b2 = b2*wd1**2;
+    b_conv = np.convolve([b0, b1, b2], [b0, b1, b2])
+    a_conv = np.convolve([1, a1, a2], [1, a1, a2])
     w, h = signal.freqz([b0, b1, b2], [1, a1, a2], plot = lambda w, h: plt.plot((fs * 0.5 / np.pi) * w, 20 * np.log10(abs(h))))
+    w, h = signal.freqz(b_conv, a_conv, plot = lambda w, h: plt.plot((fs * 0.5 / np.pi) * w, 20 * np.log10(abs(h))))
+
 
     plt.axvline(cutoff_freq, color='r')
     plt.show()
