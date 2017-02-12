46a8d773dde2209cad510a991ae835e961d1eb4d
Sam Perry <u1265119@unimail.hud.ac.uk>
2017-02-12 16:43:36 +0000

    Implemented linkwitz-riley filter

--- src/filter.h
+++ src/filter.h
@@ -5,12 +5,7 @@
 
 class Filter {
     public:
-        Filter(float crossoverFrequency, float fs, bool highpass=false, bool linkwitzRiley=false) : 
-            inputDelayBuf(3, 0), outputDelayBuf(3, 0) {
-            // Initialize delay buffer to be two samples long for the
-            // second-order Butterworth filter
-            inputDelaySize = inputDelayBuf.size();
-            outputDelaySize = outputDelayBuf.size();
+        Filter(float crossoverFrequency, float fs, bool highpass=false, bool linkwitzRiley=false) {
 
             // Calculate ratio between cutoff frequency and sampling rate
             double wc = crossoverFrequency/fs;
@@ -25,7 +20,7 @@ class Filter {
             numerator.push_back(1.0 / (1.0 + q*wd1 + pow(wd1, 2)));
             numerator.push_back(2 * numerator[0]);
             numerator.push_back(numerator[0]);
-            //denominator.push_back(1.0);
+            denominator.push_back(1.0);
             denominator.push_back(-2.0 * (pow(wd1, 2) - 1.0) * numerator[0]);
             denominator.push_back((1.0 - q * wd1 + pow(wd1, 2)) * numerator[0]);
             if(highpass) {
@@ -34,15 +29,31 @@ class Filter {
                 numerator[2] = numerator[2] * pow(wd1, 2);
             }
             if(linkwitzRiley) {
+                //rt_printf("Num size: %d\n", numerator.size());
+                //rt_printf("Den size: %d\n", denominator.size());
+                numerator = convolve(numerator, numerator);
+                denominator = convolve(denominator, denominator);
+                rt_printf("Num size: %d\n", numerator.size());
+                rt_printf("Den size: %d\n", denominator.size());
+                rt_printf("%f %f %f %f %f\n", numerator[0], numerator[1], numerator[2], numerator[3], numerator[4]);
+                rt_printf("%f %f %f %f %f\n", denominator[0], denominator[1], denominator[2], denominator[3], denominator[4]);
             }
-            rt_printf("Numerator: %f %f %f\nDenominator: %f %f\nHighpass: %s\n", 
+            else {
+            rt_printf("Numerator: %f %f %f\nDenominator: %f %f %f\nHighpass: %s\n", 
                     numerator[0], 
                     numerator[1], 
                     numerator[2], 
                     denominator[0], 
                     denominator[1],
+                    denominator[2],
                     highpass ? "true":"false");
-            
+            }
+            inputDelayBuf.assign(int(numerator.size()), 0.0);
+            outputDelayBuf.assign(int(denominator.size()), 0.0);
+            // Initialize delay buffer to be two samples long for the
+            // second-order Butterworth filter
+            inputDelaySize = inputDelayBuf.size();
+            outputDelaySize = outputDelayBuf.size();
         }
         
         float applyFilter(float x0) {
@@ -58,9 +69,10 @@ class Filter {
             for(unsigned int i = 0; i < inputDelaySize; i++) {
                 y += inputDelayBuf[(inputDelayBufWritePtr-i+inputDelaySize)%inputDelaySize] * numerator[i];
             }
-            for(unsigned int i = 0; i < outputDelaySize; i++) {
-                y -= outputDelayBuf[(outputDelayBufWritePtr-i-1+outputDelaySize)%outputDelaySize] * denominator[i];
+            for(unsigned int i = 1; i < outputDelaySize; i++) {
+                y -= outputDelayBuf[(outputDelayBufWritePtr-i+outputDelaySize)%outputDelaySize] * denominator[i];
             }
+            y /= denominator[0];
 
             outputDelayBuf[(outputDelayBufWritePtr+outputDelaySize)%outputDelaySize] = y;
 
