0bafdbf6c855b891eb11e016617bc62e65f5205d
Sam Perry <samuel.perry89@gmail.com>
2017-04-06 18:13:28 +0100

    Implemented first peak above threshold picking

--- python_prototype/main.py
+++ python_prototype/main.py
@@ -8,9 +8,12 @@ import glob
 import pdb
 import numpy as np
 import matplotlib.pyplot as plt
+import peakutils
+from peakutils.plot import plot as pplot
 from numpy.lib.stride_tricks import as_strided
 
-plotFigures = True
+plotFigures1 = False
+plotFigures2 = True
 
 
 def main():
@@ -54,7 +57,7 @@ def main():
 
         threshold = 0.5
 
-        if plotFigures:
+        if plotFigures1:
             plt.subplot(2, 1, 1)
             x = np.linspace(0, data.shape[-1], Pa.size)
             plt.plot(x, Pa)
@@ -66,8 +69,18 @@ def main():
             plt.xlim([fs*1, fs*4])
             plt.xlabel('Time (samples)')
             plt.show()
-        pdb.set_trace()
 
+        peaks = indexes(Pa, thres=threshold)
+
+        if plotFigures2:
+            x = np.linspace(0, data.shape[-1], Pa.size)
+            pplot(x, Pa, peaks)
+            plt.xlim([fs*1, fs*4])
+            plt.xlabel('Time (samples)')
+            plt.axhline(threshold, linestyle='--', color='g')
+            plt.show()
+
+        pdb.set_trace()
 
 
         # Calculate avergae shannon energy of each segment
@@ -85,6 +98,83 @@ def rolling_window(a, window, hopSize):
 
     return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
 
+
+# This function was modified from the following repository: https://bitbucket.org/lucashnegri/peakutils
+def indexes(y, thres=0.3, min_dist=1):
+    """Peak detection routine.
+
+    Finds the numeric index of the peaks in *y* by taking its first order difference. By using
+    *thres* and *min_dist* parameters, it is possible to reduce the number of
+    detected peaks. *y* must be signed.
+
+    Parameters
+    ----------
+    y : ndarray (signed)
+        1D amplitude data to search for peaks.
+    thres : float between [0., 1.]
+        Normalized threshold. Only the peaks with amplitude higher than the
+        threshold will be detected.
+    min_dist : int
+        Minimum distance between each detected peak. The peak with the highest
+        amplitude is preferred to satisfy this constraint.
+
+    Returns
+    -------
+    ndarray
+        Array containing the numeric indexes of the peaks that were detected
+    """
+    if isinstance(y, np.ndarray) and np.issubdtype(y.dtype, np.unsignedinteger):
+        raise ValueError("y must be signed")
+
+    min_dist = int(min_dist)
+
+    # compute first order difference
+    dy = np.diff(y)
+
+    # propagate left and right values successively to fill all plateau pixels (0-value)
+    zeros,=np.where(dy == 0)
+
+    while len(zeros):
+        # add pixels 2 by 2 to propagate left and right value onto the zero-value pixel
+        zerosr = np.hstack([dy[1:], 0.])
+        zerosl = np.hstack([0., dy[:-1]])
+
+        # replace 0 with right value if non zero
+        dy[zeros]=zerosr[zeros]
+        zeros,=np.where(dy == 0)
+
+        # replace 0 with left value if non zero
+        dy[zeros]=zerosl[zeros]
+        zeros,=np.where(dy == 0)
+
+    # find the peaks by using the first order difference
+    peaks = np.where((np.hstack([dy, 0.]) < 0.)
+                     & (np.hstack([0., dy]) > 0.)
+                     & (y > thres))[0]
+
+    if peaks.size > 1 and min_dist > 1:
+        highest = peaks[np.argsort(y[peaks])][::-1]
+        rem = np.ones(y.size, dtype=bool)
+        rem[peaks] = False
+
+        for peak in highest:
+            if not rem[peak]:
+                sl = slice(max(0, peak - min_dist), peak + min_dist + 1)
+                rem[sl] = True
+                rem[peak] = False
+
+        peaks = np.arange(y.size)[~rem]
+
+    # Find threshold boundaries
+    boundaries = np.where(np.diff(np.array(y > thres, dtype=int))>0)[0]
+    # Make start-end pairs of boundaries
+    boundaries = rolling_window(boundaries, 2, 1)
+    out = np.zeros(boundaries.shape[0], dtype=int)
+    # For each threshold boundary, select only the first peak
+    for ind, indexes in enumerate(boundaries):
+        out[ind] = np.min(peaks[(peaks > indexes[0]) & (peaks < indexes[1])])
+
+    return out
 
 if __name__ == "__main__":
     main()
